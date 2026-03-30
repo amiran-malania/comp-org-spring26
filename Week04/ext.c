31d01915914d3a11d04b83c161eb583fab6a202c
abstraction-builder <167092037+amiran-malania@users.noreply.github.com>
2026-03-30 09:53:44 +0400

    Week 4 lecture

---
+++ Week04/ext.c
@@ -0,0 +1,12 @@
+int main(void) {
+	short x = 15; 	// 2 bytes = 16
+	int y = x;		// 4 bytes = 32
+	
+	/**
+	 * super_short -> 4 bits
+	 * super_short x, y; 
+	 * MAX: 2^4-1 => 1111 => 15
+	 * x = 12, y = 13;
+	 * x + y = 25
+	 * 1000 1101
+}
