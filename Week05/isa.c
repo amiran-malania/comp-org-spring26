a59f044609aec612e55b370373cc50fc3e87e985
abstraction-builder <167092037+amiran-malania@users.noreply.github.com>
2026-04-06 19:32:54 +0400

    Add week 5 slides

---
+++ Week05/isa.c
@@ -0,0 +1,22 @@
+int add(int x, int y) {
+	return x + y;
+}
+
+unsigned int add(unsigned int x, unsigned int y) {
+	if (x > 0) {
+		// TODO 
+	} else {
+		// TODO 
+	}
+
+	return x + y;
+}
+
+
+
+
+int main() {
+	// ISA
+	// add(x, y);
+	add(5, 6);
+}
